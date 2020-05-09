7f1e8d2115d27520d2816a56b28038de3c2fad41
Dambr <dambr-wolf@mail.ru>
2020-05-09 16:43:25 +0300

    v1

---
+++ newro.js
@@ -0,0 +1,157 @@
+const mysql = require('mysql');
+const parameters = require('./parameters.json');
+
+// Открываем соединение с сервером
+const connection = mysql.createConnection({
+	host : 'localhost',
+	user : 'root',
+	password : 'root',
+	database : 'test',
+	insecureAuth : true,
+	port : 8889
+});
+
+let size = parameters.size; // Свойство объекта
+let countStb = parameters.parameters.length // Количество столбцов в таблице базы
+let stb = []; // Критерии создания базы в таблице базы
+let names = [];
+for (let i = 0; i < countStb; i ++){
+	stb.push(parameters.parameters[i].name + " double NOT NULL");
+	names.push(parameters.parameters[i].name);
+}
+
+
+new Promise((response, reject) => {
+	connection.query("DROP TABLE test", function(err, res){
+		if (err) throw err;
+		console.log("Прежние данные стерты");
+		connection.query("CREATE TABLE test(" +
+			"id int(11) NOT NULL, " +
+			stb.join(", ") +
+			", r double NOT NULL" +
+			", g double NOT NULL" +
+			", b double NOT NULL" + 
+			")",
+		(err, res) => {
+			if (err) throw err;
+			response();
+		});
+	});
+})
+.then(
+	() => {
+		console.log("Формат новых данных сконфигурирован");
+		main();
+	}
+)
+.then(
+	() => {
+		for (let i = 0; i < td.length; i ++){
+			let values = [];
+			for (let key of names){
+				values.push(td[i][key]);
+			}
+			connection.query("INSERT INTO test VALUES(" + td[i].id + "," + values.join(",") + "," + td[i].r + "," + td[i].g + "," + td[i].b + ")", (err, res) => {
+				if (err) throw err;
+				process.stdout.write('\r\x1b[K');
+				process.stdout.write('Записано\t\t\t' + Math.round(100 * (i + 1) / Math.pow(size, 2)) + ' %');
+				if (i == td.length - 1){
+					console.log();
+				}
+			});
+		}
+	}
+)
+.then(
+	() => {
+		connection.end();
+	}
+);
+
+
+function main(){
+	// Размер сетки и кол-во итераций обучения
+	let countOfIterations = parameters.countOfIterations;
+	// Начальные константы, влияюще на скорость и величину обучения
+	let sigma0 = parameters.sigma0;// size / 2;
+	let L0     = parameters.L0;
+
+	function randomInt(min, max) {
+		let rand = min + Math.random() * (max + 1 - min);
+		return Math.floor(rand);
+	}
+	function getTdIndex(i, j){
+		return size * i + j;
+	}
+	function getDistance(vector1, vector2){
+		let distance = 0;
+		for (let key of names){
+			distance += Math.pow((vector1[key] - vector2[key]), 2);
+		}
+		return distance;
+	}
+	function getNewralDistance(koord1, koord2){
+		let distance = 0;
+		distance += Math.pow((koord1[0] - koord2[0]), 2);
+		distance += Math.pow((koord1[1] - koord2[1]), 2);
+		return distance;
+	}
+
+	// Шаг 1
+	// Создаем поле и инициализируем его
+	// Глобальная переменная td
+	td = [];
+	for (let i = 0; i < size; i ++){
+		for (let j = 0; j < size; j ++){
+			let _td = {};
+			_td.id = getTdIndex(i, j);
+			for (let key = 0; key < parameters.parameters.length; key ++){
+				_td[parameters.parameters[key].name] = randomInt(parameters.parameters[key].min, parameters.parameters[key].max);
+			}
+			_td.r = 255;
+			_td.g = 255;
+			_td.b = 255;
+			td.push(_td);
+		}
+	}
+
+	let master_vector = require('./master_vector.json');
+
+	// Обучение сети
+	
+	for (let k = 1; k < countOfIterations; k ++){
+		process.stdout.write('\r\x1b[K');
+		process.stdout.write('Обучено\t\t\t\t' + Math.round(100 * (k + 1) / countOfIterations) + ' %');
+		if (k == countOfIterations - 1){
+			console.log();
+		}
+		let distances = [];
+		let koord     = [];
+		for (let i = 0; i < size; i ++){
+			for (let j = 0; j < size; j ++){
+				distances.push(getDistance(master_vector[k % master_vector.length], td[getTdIndex(i, j)]));
+				koord.push([i, j]);
+			}
+		}
+
+		let minDistance = Math.min(...distances);
+		let minKoord    = koord[distances.indexOf(minDistance)];
+
+		let lamda = k / Math.log(sigma0);
+		let sigma = sigma0 * Math.exp( -k / lamda);
+		let L     = L0 * Math.exp( -k / lamda);
+
+		for (let i = 0; i < size; i ++){
+			for (let j = 0; j < size; j ++){
+				let teta  = Math.exp( -getNewralDistance(minKoord, [i, j]) / (2 * Math.pow(sigma, 2)));
+				for (let key of names){
+					td[getTdIndex(i, j)][key] += teta * L * (master_vector[k % master_vector.length][key] - td[getTdIndex(i, j)][key]);
+				}
+				
+				td[getTdIndex(i, j)].r += teta * L * (master_vector[k % master_vector.length].r - td[getTdIndex(i, j)].r);
+				td[getTdIndex(i, j)].g += teta * L * (master_vector[k % master_vector.length].g - td[getTdIndex(i, j)].g);
+				td[getTdIndex(i, j)].b += teta * L * (master_vector[k % master_vector.length].b - td[getTdIndex(i, j)].b); 
+			}
+		}
+	}
+}
---
+++ public/script/index.js
@@ -0,0 +1,176 @@
+
+let text = JSON.parse(document.getElementById("text").textContent);
+let size = Number(document.getElementById("size").textContent);
+let parameters = JSON.parse(document.getElementById("parameters").textContent);
+
+function getTdIndex(i, j){
+	return size * i + j;
+}
+function getColor(r, g, b){
+	return 'rgb(' + r + ',' + g + ',' + b + ')';
+}
+function drawTdColor(i, j, color){
+	document.getElementsByTagName('td')[getTdIndex(i, j)].style.backgroundColor = color;
+}
+function getDistance(vector1, vector2){
+	let distance = 0;
+	for (let key of names){
+		distance += Math.pow((vector1[key] - vector2[key]), 2);
+	}
+	return distance;
+}
+
+function getColorLength(color){
+	let len = 0;
+	for (let i = 0; i < color.length; i ++){
+		len += Math.pow(color[i], 2);
+	}
+	return Math.sqrt(len);
+}
+
+function getColorDeg(color1, color2){
+	let deg = 0;
+	for (let i = 0; i < color1.length; i ++){
+		deg += (color1[i] * color2[i]) / (getColorLength(color1) * getColorLength(color2));
+	}
+	return deg;
+}
+
+function getColorDistance(color1, color2){
+	let distance = 0;
+	for (let i = 0; i < color1.length; i ++){
+		distance += Math.pow((color1[i] - color2[i]), 2);
+	}
+	return Math.sqrt(distance);
+}
+
+function isEqualVectors(vector1, vector2){
+	let len = Math.max(vector1.length, vector2.length);
+	let sorted1 = vector1.slice().sort();
+	let sorted2 = vector2.slice().sort();
+	for (let i = 0; i < len; i ++){
+		if (sorted1[i] !== sorted2[i]){
+			return false;
+		}
+	}
+	return true;
+}
+
+function getColorVector(color){
+	let colorVector = color.replace('rgb(', '').replace(')', '').split(', ');
+	for (let i = 0; i < colorVector.length; i ++){
+		colorVector[i] = Number(colorVector[i]);
+	}
+	return colorVector;
+}
+
+function likeMasterVector(color){
+	let colorVector = getColorVector(color);
+	if (isEqualVectors(colorVector, [255, 255, 255])){
+		return 'Неопределено';
+	}
+	let table = document.getElementById('master_vector');
+	let tr = table.getElementsByTagName('tr');
+	let master_color = [];
+	for (let i = 0; i < tr.length; i ++){
+		master_color.push(getColorVector(document.getElementById('master_vector').getElementsByTagName('tr')[i].getElementsByTagName('td')[1].style.backgroundColor));
+	}
+	let distances = [];
+	for (let i = 0; i < tr.length; i ++){
+		distances.push(getColorDeg(colorVector, master_color[i]));
+	}
+	let minDistance = Math.max(...distances);
+	let minIndex    = distances.indexOf(minDistance);
+
+	console.log(color, distances);
+
+	return document.getElementById('master_vector').getElementsByTagName('tr')[minIndex].getElementsByTagName('td')[0].textContent;
+}
+
+function projection(vector){
+	for (let i = 0; i < td.length; i ++){
+		document.getElementsByTagName('td')[i].style.borderColor = 'white';
+	}
+	let distances = [];
+	let koord     = [];
+	for (let i = 0; i < size; i ++){
+		for (let j = 0; j < size; j ++){
+			distances.push(getDistance(vector, td[getTdIndex(i, j)]));
+			koord.push([i, j]);
+		}
+	}
+	let minDistance = Math.min(...distances);
+	let minKoord    = koord[distances.indexOf(minDistance)];
+	document.getElementsByTagName('td')[getTdIndex(...minKoord)].style.borderColor = 'white';
+
+	let problem = likeMasterVector(document.getElementsByTagName('td')[getTdIndex(...minKoord)].style.backgroundColor);
+
+	let solution = JSON.parse(document.getElementById('solution').textContent);
+	
+	if (problem != 'Неопределено'){
+		let index = solution.problem.indexOf(problem);
+		let values = [];
+		for (let i = 0; i < solution.solution.length; i ++){
+			values.push(solution.solution[i].value);
+		}
+		let array = [];
+		for (let i = 0; i < solution.solution.length; i ++){
+			array.push(values[i][index]);
+		}
+		solution = 'Решение: ' + solution.solution[array.indexOf(Math.max(...array))].name;
+	}
+	else{
+		solution = '';
+	}
+	document.getElementById('output').getElementsByTagName('label')[0].textContent = 'Проблема: ' + problem;
+	document.getElementById('output').getElementsByTagName('label')[1].textContent = solution;
+}
+
+// Создаем поле и инициализируем его
+let names = [];
+for (let i = 0; i < parameters.parameters.length; i ++){
+	names.push(parameters.parameters[i].name);
+}
+let td = [];
+for (let i = 0; i < size; i ++){
+	let tr = document.createElement('tr');
+	for (let j = 0; j < size; j ++){
+		let _td = {};
+		_td.id = getTdIndex(i, j);
+		for (let key = 0; key < parameters.parameters.length; key ++){
+			_td[parameters.parameters[key].name] = 0;
+		}
+		_td.r = 255;
+		_td.g = 255;
+		_td.b = 255;
+		_td.element = document.createElement('td');
+		td.push(_td);
+		tr.appendChild(td[td.length - 1].element);
+	}
+	document.getElementById('table').appendChild(tr);
+}
+
+for (let i = 0; i < size; i ++){
+	for (let j = 0; j < size; j ++){
+		let title = [];
+		for (let key of names){
+			td[text[getTdIndex(i, j)].id][key] = text[getTdIndex(i, j)][key];
+			title.push(key + ": " + text[getTdIndex(i, j)][key]);
+		}
+		td[text[getTdIndex(i, j)].id].r = text[getTdIndex(i, j)].r;
+		td[text[getTdIndex(i, j)].id].g = text[getTdIndex(i, j)].g;
+		td[text[getTdIndex(i, j)].id].b = text[getTdIndex(i, j)].b;
+
+		document.getElementsByTagName('td')[text[getTdIndex(i, j)].id].title = title.join("\n");
+	}
+}
+
+// Покраска поля
+for (let i = 0; i < size; i ++){
+	for (let j = 0; j < size; j ++){
+		drawTdColor(i, j, getColor(td[getTdIndex(i, j)].r, td[getTdIndex(i, j)].g, td[getTdIndex(i, j)].b));
+	}
+}
+
+document.getElementById("image").style.width = document.getElementById("table").offsetWidth + "px";
+document.getElementById("image").style.height = document.getElementById("table").offsetHeight + "px";
